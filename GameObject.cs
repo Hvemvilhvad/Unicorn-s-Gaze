3f121c77c93fcfdbb798f2edb4f2a600189947c7
Hvemvilhvad <malthewbirch@gmail.com>
2024-11-19 12:41:43 +0100

    check coolision

--- GameObject.cs
+++ GameObject.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Unicorns_Gaze
 {
-    public class GameObject
-{
+    public abstract class GameObject
+    {
+        Rectangle hitbox;
+
+        public Rectangle Hitbox { get => hitbox; set => hitbox = value; }
+
         public void Update()
         {
             //add update logic here
+
         }
 
         public void CheckCollision(GameObject other)
         {
             //add collision logic here
+            if (Hitbox.IntersectsWith(other.Hitbox))
+            {
+                OnCollision(other);
+            }
         }
-}
+
+        public void OnCollision(GameObject other)
+        {
+
+        }
+    }
 }
