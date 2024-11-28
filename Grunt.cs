9bec826bf384b13f40db22ae49c1acc09efd850c
Eleavate <echoovervad@gmail.com>
2024-11-28 13:52:22 +0100

    Added Grunt attack

--- Grunt.cs
+++ Grunt.cs
@@ -11,6 +11,9 @@ namespace Unicorns_Gaze
 {
     public class Grunt : Enemy
     {
+        //Fields
+        
+
         //Constructor
         public Grunt(int health, Vector2 position, float speed) : base(health, position, speed)
         {
@@ -22,7 +25,7 @@ namespace Unicorns_Gaze
         public Grunt(Vector2 position) : base(position)
         {
             Health = 10;
-            speed = 250;
+            speed = 150;
         }
 
         public override void LoadContent(ContentManager content)
@@ -40,13 +43,39 @@ namespace Unicorns_Gaze
         public override void Update(GameTime gameTime, Vector2 screenSize)
         {
             Chase();
+            cooldown -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            attackCooldown -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (cooldown <= 0)
+            {
+                cooldown = 0;
+            }
             base.Update(gameTime, screenSize);
         }
 
         public override void OnCollision(GameObject other)
         {
+            if (other is Player)
+            {
+                velocity = Vector2.Zero;
+                cooldown = 2;
+                if (attackCooldown <= 0)
+                {
+                    Attack();
+                }
+                
+            }
             base.OnCollision(other);
         }
-        
+        /// <summary>
+        /// Override of the chase method, allowing the enemy to stop if it collides with the player
+        /// </summary>
+        public override void Chase()
+        {
+            if (cooldown <= 0)
+            {
+                base.Chase();
+            }
+        }
+
     }
 }
