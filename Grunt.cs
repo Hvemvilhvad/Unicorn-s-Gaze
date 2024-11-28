1ffbf34d7c7c99c72c9e0d74659ad5f9b6b6f309
Eleavate <echoovervad@gmail.com>
2024-11-28 10:47:53 +0100

    Added Grunt constructor and stuff

--- Grunt.cs
+++ Grunt.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,32 @@ namespace Unicorns_Gaze
 {
     public class Grunt : Enemy
     {
-        //Fields
+        //Constructor
+        public Grunt(int health, Vector2 position, float speed) : base(health, position, speed)
+        {
+            Health = health;
+            Position = position;
+            this.speed = speed;
+        }
+      
+        public Grunt(Vector2 position) : base(position)
+        {
+            Position = position;
+        }
 
+        public override void LoadContent(ContentManager content)
+        {
+            base.LoadContent(content);
+        }
 
+        public override void Update(GameTime gameTime, Vector2 screenSize)
+        {
+            base.Update(gameTime, screenSize);
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+            base.OnCollision(other);
+        }
     }
 }
