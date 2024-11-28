f728c65c1b3725e2e67e903e391aef3b9ac4d9af
Eleavate <echoovervad@gmail.com>
2024-11-28 12:36:39 +0100

    IDamagable Added

---
+++ IDamagable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicorns_Gaze
+{
+    internal interface IDamagable
+    {
+        public int Health { get; set; }
+
+        /// <summary>
+        /// Lowers the health of the Damagable when it takes damage.
+        /// </summary>
+        /// <param name="damage">The amount to lower it by.</param>
+        void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+    }
+}
