d4da213cb78d64edb1d5d7f5917a1acac4ff16c4
Hvemvilhvad <malthewbirch@gmail.com>
2024-12-02 14:20:12 +0100

    switchin' pc's

--- Character.cs
+++ Character.cs
@@ -82,6 +82,7 @@ namespace Unicorns_Gaze
         protected bool isFacingRight;
         protected Texture2D attackSprite;
         protected float attackCooldown = 0.5f;
+        protected int
 
 
         //Properties
