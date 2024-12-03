1932c52241f23e1562293c435c1922093f0e24da
Eleavate <echoovervad@gmail.com>
2024-12-03 13:51:24 +0100

    Splash text implement

---
+++ SplashText.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicorns_Gaze
+{
+    internal class SplashText : GameObject
+    {
+        private SpriteFont splashFont;
+        private string text;
+        private Color textColor;
+        private GameObject target;
+        private float textTimer = 2;
+        public string Text { get => text; set => text = value; }
+        public Color TextColor { get => textColor; set => textColor = value; }
+        public GameObject Target { get => target; set => target = value; }
+
+        public SplashText(string text, Color color, GameObject target)
+        {
+            Text = text;
+            TextColor = color;
+            Target = target;
+            Position = target.Position + new Vector2(0, -100);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            splashFont = content.Load<SpriteFont>("textfont_ui");
+            base.LoadContent(content);
+        }
+
+        public override void Update(GameTime gameTime, Vector2 screenSize)
+        {
+            textTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (textTimer <= 0)
+            {
+                RemoveThis();
+            }
+            base.Update(gameTime, screenSize);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(splashFont, Text, Position, TextColor);
+        }
+    }
+}
